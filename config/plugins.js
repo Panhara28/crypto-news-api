7e9af3c900776583b8bdbcb91b4c6b4f895867ca
aipanhara <mcpanhara@gmail.com>
2023-06-17 18:37:48 +0700

    fix

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,19 @@
-module.exports = ({ env }) => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      sizeLimit: 250 * 1024 * 1024, // 256mb in bytes
+      breakpoints: {
+        xlarge: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xsmall: 64,
+      },
+      providerOptions: {
+        localServer: {
+          maxage: 300000,
+        },
+      },
+    },
+  },
+});
